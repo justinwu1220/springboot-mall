899bfd0712aa0a4476edc84ab8dc6bf663789214
JustinWu <z891220z@gmail.com>
2024-04-26 10:23:37 +0800

    訂單功能 - ADMIN權限查詢所有訂單

--- src/main/java/com/justinwu/springbootmall/controller/OrderController.java
+++ src/main/java/com/justinwu/springbootmall/controller/OrderController.java
@@ -1,10 +1,13 @@
 package com.justinwu.springbootmall.controller;
 
+import com.justinwu.springbootmall.constant.Authority;
 import com.justinwu.springbootmall.dto.CreateOrderRequest;
 import com.justinwu.springbootmall.dto.OrderQueryParams;
 import com.justinwu.springbootmall.model.Order;
 import com.justinwu.springbootmall.model.Product;
+import com.justinwu.springbootmall.model.User;
 import com.justinwu.springbootmall.service.OrderService;
+import com.justinwu.springbootmall.service.UserService;
 import com.justinwu.springbootmall.tool.UserLoginToken;
 import com.justinwu.springbootmall.util.JwtUtil;
 import com.justinwu.springbootmall.util.Page;
@@ -16,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -24,6 +28,8 @@ public class OrderController {
 
     @Autowired
     private OrderService orderService;
+    @Autowired
+    private UserService userService;
 
     @UserLoginToken//需要登入權限
     @GetMapping("/users/orders")
@@ -36,8 +42,14 @@ public class OrderController {
         Claims claims = JwtUtil.parseJwtToken(token);
         Integer userId = Integer.valueOf(claims.getId());
 
+        User user = userService.getUserById(userId);
+
         OrderQueryParams orderQueryParams = new OrderQueryParams();
-        orderQueryParams.setUserId(userId);
+
+        //若為CLIENT則找該CLIENT訂單，若為ADMIN則找全部訂單
+        if(user.getAuthority().equals(Authority.CLIENT)){
+            orderQueryParams.setUserId(userId);
+        }
         orderQueryParams.setLimit(limit);
         orderQueryParams.setOffset(offset);
 
